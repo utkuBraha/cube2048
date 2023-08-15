324e9a8f9062d33658e84c90c3046f558d144ca4
utkuBraha <84633839+utkuBraha@users.noreply.github.com>
2023-08-15 18:11:32 +0300

    version 0.3

--- Assets/Scripts/ForceYMove.cs
+++ Assets/Scripts/ForceYMove.cs
@@ -1,55 +1,45 @@
 using System;
 using ChainCube.Scripts.Utils;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace ChainCube.Scripts.Handlers
 {
     public class ForceYMovementSwipeHandler : MonoBehaviour, IMovableObject
     {
-        [SerializeField]
-        private float _force = 1.0f;
-        
+        [SerializeField] private float force = 1.0f;
         private Rigidbody moveRb;
         private ISwipeDetector swipeDetector;
-        
         public void Inject(GameObject dependency)
         {
             moveRb = dependency.GetComponent<Rigidbody>();
         }
-
         private void Start()
         {
             swipeDetector = GetComponent<MouseSwipeDetector>();
             Subscribe();
         }
-
         private void Subscribe()
         {
             if (swipeDetector == null)
                 throw new NullReferenceException();
-
             swipeDetector.onSwipeEnd += OnSwipeEnd;
         }
-
         private void OnSwipeEnd(Vector2 delta)
         {
             if (moveRb == null)
                 return;
-            
-            moveRb.AddForce(moveRb.transform.forward * _force, ForceMode.Impulse);
+            moveRb.AddForce(moveRb.transform.forward * force, ForceMode.Impulse);
             moveRb = null;
         }
-
         private void OnDestroy()
         {
             Unsubscribe();
         }
-
         private void Unsubscribe()
         {
             if (swipeDetector == null)
                 return;
-
             swipeDetector.onSwipeEnd -= OnSwipeEnd;
         }
     }
--- Assets/Scripts/XMovementHandler.cs
+++ Assets/Scripts/XMovementHandler.cs
@@ -7,51 +7,39 @@ namespace ChainCube.Scripts.Handlers
 {
     public class XMovementHandler : MonoBehaviour, IMovableObject
     {
-      
         [SerializeField]
         private Transform leftBorder;
-        
         [SerializeField]
         private Transform rightBorder;
-
         [SerializeField, Range (0.5f, 1.5f)]
         private float normalizedCoefficient = 1.0f;
-        
         private GameObject movableObject;
-
         private ISwipeDetector swipeDetector;
         
         public void Inject(GameObject dependency)
         {
             movableObject = dependency;
         }
-        
         private void Start()
         {
             swipeDetector = GetComponent<ISwipeDetector>();
             Subscribe();
         }
-
         private void Subscribe()
         {
             if (swipeDetector == null)
-
-                if (swipeDetector != null)
-                    swipeDetector.onSwipe += OnSwipe;
-            if (swipeDetector != null) 
-                swipeDetector.onSwipeEnd += OnSwipeEnd;
+                throw new NullReferenceException();
+            swipeDetector.onSwipe += OnSwipe;
+            swipeDetector.onSwipeEnd += OnSwipeEnd;
         }
-
         private void OnSwipe(Vector2 delta)
         {
             if (movableObject == null)
             {
                 return;
             }
-
             if (Mathf.Abs(delta.x - Mathf.Epsilon) <= 0)
                 return;
-
             var borderDistance = Mathf.Abs(rightBorder.position.x - leftBorder.position.x);
             var offset = borderDistance * normalizedCoefficient * delta.x / Screen.width;
             var currentPos = movableObject.transform.position;
@@ -63,25 +51,20 @@ namespace ChainCube.Scripts.Handlers
             else if (movableObject.transform.position.x < leftBorder.position.x)
                 movableObject.transform.position = leftBorder.transform.position;
         }
-
         private void OnSwipeEnd(Vector2 delta)
         {
             movableObject = null;
         }
-
         private void OnDestroy()
         {
             Unsubscribe();
         }
-
         private void Unsubscribe()
         {
             if (swipeDetector == null)
                 return;
-            
             swipeDetector.onSwipe -= OnSwipe;
         }
     }
     public interface IMovableObject : IDependency<GameObject> { }
 }
-
